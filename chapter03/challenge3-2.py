cf26a374d4cde22e8d12abdc2811d5b7cc86bfee
itc-s25002 <s25002@std.it-college.ac.jp>
2025-05-20 06:49:54 +0000

    P48　チャレンジ問題終了

---
+++ chapter03/challenge3-2.py
@@ -0,0 +1,6 @@
+number = 10
+
+if number < 10:
+    print("less than 10")
+elif number >= 10:
+    print("10 or more")
---
+++ chapter03/challenge3-3.py
@@ -0,0 +1,8 @@
+number = 14
+
+if number <= 10:
+    print("10以下")
+elif number > 10 and number <= 25:
+    print("10より大きく25以下")
+elif number >25:
+    print("25より大きい")

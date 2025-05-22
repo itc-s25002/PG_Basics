a306d35cff938a64528d285d66570fb4cc0ccafc
itc-s25002 <s25002@std.it-college.ac.jp>
2025-05-22 06:20:19 +0000

    challenge3-3を修正

--- chapter03/challenge3-3.py
+++ chapter03/challenge3-3.py
@@ -1,8 +1,8 @@
-number = 14
+number = 62
 
 if number <= 10:
     print("10以下")
-elif number > 10 and number <= 25:
+elif number <= 25:
     print("10より大きく25以下")
 elif number >25:
     print("25より大きい")

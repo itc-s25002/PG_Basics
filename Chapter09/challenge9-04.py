d9dff17b462f8fd6d977a0f2db7011b1ba42cb5d
itc-s25002 <s25002@std.it-college.ac.jp>
2025-06-18 00:44:22 +0000

    第９章　チャレンジ問題終了

--- Chapter09/challenge9-04.py
+++ Chapter09/challenge9-04.py
@@ -1,6 +1,6 @@
 import csv
 title =[["トップガン","卒業白書","マイノリティーリポート"],["タイタニック","レヴェナント","インセプション"],["トレーニング デイ","マイ・ボディーガード","フライト"]]
-with open("movie.csv","w",newline="") as f:
+with open("movie.csv","w",newline="",encoding="utf-8") as f:
     w = csv.writer(f,delimiter=",")
     for i in title:
         w.writerow(i)

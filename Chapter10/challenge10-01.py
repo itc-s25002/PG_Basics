07f17a5ae5ac8aafe1e16ef0d074dfab4e3907ab
itc-s25002 <s25002@std.it-college.ac.jp>
2025-06-19 02:33:15 +0000

    第１０章　チャレンジ問題終了

---
+++ Chapter10/challenge10-01.py
@@ -0,0 +1,40 @@
+def hangman(word):
+    wrong = 0
+    stage = ["",
+             "ーーーーーー       ",
+             "|                  ",
+             "|          |       ",
+             "|          O       ",
+             "|         /|\\     ",
+             "|         / \\     "
+             ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ")
+    while wrong < len(stage) - 1:
+        print("\n")
+        msg = "１文字を予想してね: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stage[0:e]))
+        if "_" not in board:
+            print("あなたの勝ち！")
+            win = True
+            break
+    if not win:
+        print("\n".join(stage[0:wrong+1]))
+        print("あなたの負け！正解は{}.".format(word))
+
+import random
+
+word_list = ["cat","dog","cut","dig","fun","sun","run","gas","bas","bat","but"]
+answer = random.choice(word_list)
+hangman(answer)
